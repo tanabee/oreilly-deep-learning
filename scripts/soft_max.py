8c2861a8552474eca18e207f7841ae7f2db6b71c
tanabee <tanabee1986@gmail.com>
2017-05-11 22:12:34 +0900

    オーバーフロー対策

--- scripts/soft_max.py
+++ scripts/soft_max.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 def soft_max(a):
-    exp_a = np.exp(a)
+    c = np.max(a)
+    exp_a = np.exp(a - c)
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
 
     return y
 
-Y = soft_max(np.array([0.3, 2.9, 4.0]))
-print(Y)
+y = soft_max(np.array([0.3, 2.9, 4.0]))
+
+print(y)
+print(np.sum(y))

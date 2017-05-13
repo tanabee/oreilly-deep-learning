5b0ac35f46d4009dc26660cc9f0ad7cb8d93b60b
tanabee <tanabee1986@gmail.com>
2017-05-13 20:55:07 +0900

    勾配法

---
+++ scripts/gradient_descent.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+def numerical_gradient(f, x):
+    h = 1e-4
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x)
+
+        x[idx] = tmp_val -h
+        fxh2 = f(x)
+
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        x[idx] = tmp_val
+
+    return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+def function_2(x):
+    return np.sum(x**2)
+
+init_x = np.array([-3.0, 4.0])
+result = gradient_descent(function_2, init_x=init_x, lr=0.1, step_num=100)
+
+print(result)
